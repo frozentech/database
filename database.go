ec416a7b47b4cb65764b0e5c4f06fd02690b4edd
Lester Soriano <lestersoriano@reddotpay.com>
2021-01-22 09:36:22 +0800

    Database

--- database.go
+++ database.go
@@ -11,15 +11,15 @@ const (
 	MYSQLTimestampFormat = `2006-01-02 15:04:05`
 )
 
-// Client ...
+// client ...
 var (
-	Client *Database
+	client *Database
 )
 
 // Database ...
 type Database struct {
-	Connection *sqlx.DB
-	IsClose    bool
+	connection *sqlx.DB
+	isClose    bool
 }
 
 // New ...
@@ -36,28 +36,47 @@ func (me *Database) Connect(username string, password, host string, port string,
 		port,
 		dbname)
 
-	me.Connection, err = sqlx.Connect(engine, connection)
+	me.connection, err = sqlx.Connect(engine, connection)
 	return
 }
 
 // GetConnection ...
 func (me *Database) GetConnection() *sqlx.DB {
-	return me.Connection
+	return me.connection
+}
+
+// IsClose ...
+func (me *Database) IsClose() bool {
+	if me.connection == nil || me.isClose {
+		return true
+	}
+	return false
+}
+
+// Destroy ...
+func (me *Database) Destroy() {
+	me.connection.Close()
+	me.isClose = true
+	return
 }
 
 // Destroy connection
 func Destroy() {
-	if Client != nil {
-		Client.Connection.Close()
-		Client.IsClose = true
+	if client != nil {
+		client.Destroy()
 	}
+}
+
+// GetConnection ...
+func GetConnection() *sqlx.DB {
+	return client.GetConnection()
 }
 
 // Connect to database
 func Connect(username, password, host, port, dbname, engine string) (err error) {
-	if Client == nil || Client.IsClose || Client.Connection == nil {
-		Client = New()
-		err = Client.Connect(username, password, host, port, dbname, engine)
+	if client == nil || client.IsClose() {
+		client = New()
+		err = client.Connect(username, password, host, port, dbname, engine)
 		return
 	}
 
